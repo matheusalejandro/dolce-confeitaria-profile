a7acdeb65f77cf4dbbe1829f530d29c57226da44
MDALEJAN <MDALEJAN@MDALEJAN-T470.br.oracle.com>
2021-02-20 01:41:31 -0300

    Versão 2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  socialNetworks: Array<any>;
+  isDesc: boolean = false;
+  column: string = 'order';
+  constructor() { }
+
   title = 'Dolce Confeitaria';
+
+  ngOnInit() {
+    this.socialNetworks = [
+      {
+        order: 2,
+        name: 'Instagram',
+        icon: 'instagram.svg',
+        link: 'https://instagram.com/dolce_confeitaria'
+      },
+      {
+        order: 1,
+        name: 'TikTok',
+        icon: 'tiktok.svg',
+        link: 'https://www.tiktok.com/@mayaragaspar'
+      },
+      {
+        order: 4,
+        name: 'Facebook',
+        icon: 'facebook.svg',
+        link: 'https://www.facebook.com/DolceConfeitariaSP'
+      },
+      {
+        order: 3,
+        name: 'Whatsapp',
+        icon: 'whatsapp.svg',
+        link: 'https://wa.me/%2B5511987797059?text=Ol%C3%A1!%20Gostaria%20de%20fazer%20um%20pedido.'
+      }
+    ]
+  }
 
   public open(item) {
     console.log("teste");
     window.location.href=item;
   }
+
+  sort(property){
+    this.isDesc = !this.isDesc; //change the direction
+    this.column = property;
+    let direction = this.isDesc ? 1 : -1;
+
+    this.socialNetworks.sort(function(a, b){
+        if(a[property] < b[property]){
+            return -1 * direction;
+        }
+        else if( a[property] > b[property]){
+            return 1 * direction;
+        }
+        else{
+            return 0;
+        }
+    });
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OrderrByPipe } from './orderby';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, OrderrByPipe],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
